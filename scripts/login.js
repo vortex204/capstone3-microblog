2a1dcdf51f184536450748b7bb4f305006fdc502
Yonesh Thapa <hsenoy2022@gmail.com>
2023-06-29 11:59:16 -0400

    login.js created

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+
+document.getElementById("loginButton").addEventListener("click", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const loginData = {
+      username: username,
+      password: password
+    };
+    login(loginData);
+    });
